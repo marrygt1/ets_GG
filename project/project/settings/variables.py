d19e059aa2365afcd7b0b64072e974d64c505ea4
flipjack <rampzodia1@gmail.com>
2014-07-07 13:16:07 -0500

    Actualizar fotos y variables

---
+++ project/project/settings/variables.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import datetime
+ 
+EMPRESA = 'ETS_GG'
+EXPIRA = datetime.date(2099, 6, 1)
+NOMBRE = "Guadalupe Gandarilla"
+#######
+SECRET_KEY_PRIV = 'p*v33kjgu^_s!^fe+-_g^fiv5^@e377647!m&3%%xa6!bvbdy$'
+NOMBRE_DB = 'xxxx'
+PASSWORD_DB_PRIV = 'xxxx'
+EMAIL_USUARIO_PRIV = 'xxx'
+EMAIL_PASSWORD_PRIV = 'xxx'
